da20e498da17f82e71d9276050767d698f3bf3f5
jAndres-Ortiz02 <jagomezo02@libertadores.edu.co>
2025-11-05 22:21:38 -0500

    creando rama catalogo-inventario

--- backend/catalogoInventario-api/src/app.js
+++ backend/catalogoInventario-api/src/app.js
@@ -2,7 +2,7 @@ import express from "express"; // si usas módulos ES (type: module)
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola desde Node.js!");
+  res.send("Holaaa desde Node.js!");
 });
 
 app.listen(3000, () => console.log("Servidor escuchando en http://localhost:3000"));
