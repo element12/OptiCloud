f6b48b65fd167e6a7c7a18e54a66e398b6fd60bf
Murload7 <a.murcia@montechelo.online>
2025-11-06 00:55:35 -0500

    Creación de ambiente, configuración de contenedor local

---
+++ backend/historialOptometrico-api/src/db.js
@@ -0,0 +1,8 @@
+import pg from "pg";
+ 
+export const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Azure PG exige TLS; para demo deshabilitamos validación de CA
+});
+
+console.log("✅ Connected Database Postgresql ");
